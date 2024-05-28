e91a370ec2b7ccfcd96afc716c22865e6ddd9d88
thnguyen2 <thang.nguyen@silabs.com>
2024-05-28 10:55:19 +0700

    move css file to assets. create images folder in assets

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import '@/assets/css/style.css'
 import App from './App.vue'
 import router from "./router";
-// import { store } from "./store";
+import { store } from './store';
 
 import "bootstrap/scss/bootstrap.scss";
-import { store } from './store';
 
 createApp(App).use(store).use(router).mount('#app')
